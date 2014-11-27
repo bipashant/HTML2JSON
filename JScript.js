fc1d28c0d90d2ed331fc550733e10d25c655c63e
bibek <bipashant@gmail.com>
2014-11-27 06:39:21 +0545

    Final Commit

---
+++ JScript.js
@@ -0,0 +1,26 @@
+﻿var json = "";
+var mainwrapper = document.getElementsByTagName('div')[0];
+parseHTML(mainwrapper);
+console.log(json);
+
+function parseHTML(currentnode) {
+    json += ("{");
+    json += ('tag: "' + currentnode.tagName + '"');
+    
+    for (var j = 0; j < currentnode.attributes.length; j++) {
+        json += (',' + currentnode.attributes[j].name + ': "' + currentnode.attributes[j].value + '"');
+    }
+    
+    if (currentnode.children.length > 1) {
+        json += (', "children" : [');
+        for (var i = 0; i < currentnode.children.length; i++) {
+            if (i > 0)
+                json += (",");
+            parseHTML(currentnode.children[i]);
+        }
+        json += ("]");
+    }
+
+    json += ("}");
+
+}
